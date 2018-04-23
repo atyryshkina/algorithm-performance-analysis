beb67601d0aaa7e3bb6209ecf198758a1aa3e093
Anastasia Tyryshkina <atyryshkina@gmail.com>
2018-04-23 13:05:36 -0400

    saving model to pkl

---
+++ preprocessing.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from sklearn_pandas import gen_features
+import sklearn
+from sklearn_pandas import DataFrameMapper
+
+class ChooseFeatureColumns():
+    def __init__(self):
+        self.num_cols=[] 
+        self.cat_cols=[]
+        
+    def fit(self,X,y=None):
+        for col in X:
+            if X[col].dtype == float:
+                self.num_cols.append(col)
+            else:
+                self.cat_cols.append(col)
+        return self
+    
+    def transform(self,X, y=None):
+        X = X.copy()
+        X = X.reset_index(drop=True)
+        newX = pd.DataFrame(index=np.arange(X.shape[0]))
+        
+        for col in self.num_cols:
+            if col in X:
+                newX[col] = X[col].astype(float)
+                newX[col]=newX[col].fillna(0)
+            else:
+                print("%s not in dataframe - adding empty column" % col)
+                newX[col]=np.nan
+                newX[col]=newX[col].astype(float)
+                newX[col]=newX[col].fillna(0)
+                
+        for col in self.cat_cols:
+            if col in X:
+                newX[col] = X[col].astype(str)
+            else:
+                print("%s not in dataframe - adding empty column" % col)
+                newX[col]=np.nan
+                newX[col]=newX[col].astype(str)
+        
+        return newX
+    
+    def fit_transform(self,X, y=None):
+        self.fit(X)
+        return self.transform(X)
+
+class MyMapper():
+    def __init__(self):
+        self.ncols = []
+        self.scols = []
+        
+    def fit(self,X,y=None):
+        for col in X:
+            if X[col].dtype == float:
+                print("numerical col: %s" % col)
+                self.ncols.append([col])
+            else:
+                print("categorical col: %s" % col)
+                self.scols.append([col])
+        nfeats = gen_features(
+              columns=self.ncols,
+              classes=[{'class':sklearn.preprocessing.MinMaxScaler,}]  
+        )
+        sfeats = gen_features(
+              columns=self.scols,
+              classes=[{'class':sklearn.preprocessing.LabelBinarizer}]  
+        )
+        self.mapper = DataFrameMapper(nfeats+sfeats,df_out=True)
+        self.mapper.fit(X)
+        return self
+    
+    def transform(self,X, y=None):
+        X = X.copy()
+        X = self.mapper.transform(X)
+        return X
+    
+    def fit_transform(self,X, y=None):
+        self.fit(X)
+        return self.transform(X)
---
+++ train_model.py
@@ -0,0 +1,115 @@
+import argparse
+import scipy
+from sklearn.externals import joblib
+
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn_pandas import gen_features
+from sklearn_pandas import DataFrameMapper
+
+from sklearn import ensemble
+import sklearn
+import numpy as np
+from matplotlib import pyplot as plt
+from preprocessing import ChooseFeatureColumns
+from preprocessing import MyMapper
+
+class TrainModel(object):
+
+    def __init__(self, args):
+
+        self.args = args
+
+        # load the input file into a pandas dataframe
+        if args.filename.endswith(".csv"):
+            self.df=pd.read_csv(args.filename)
+        elif args.filename.endswith(".tsv"):
+            self.df=pd.read_csv(args.filename, sep="\t")
+        else:
+            raise ValueError("unrecognized filetype: %s. I only accept tsv or csv files" % self.args.filename)
+
+        df_features, df_labels = self.df, self.df.pop(self.args.runtime_label)
+        df_labels = np.log1p(df_labels)
+
+        # prepare the data for the RandomForestRegressor
+        print("setting up...")
+        chooser = ChooseFeatureColumns()
+        scaler = MyMapper()
+        regr = sklearn.ensemble.RandomForestRegressor(n_estimators=100, max_depth=12)
+
+        self.pipe = sklearn.pipeline.Pipeline([
+            ('chooser',chooser),
+            ('scaler', scaler),
+            ('regr', regr)
+        ])
+        
+        test_size = 0.2
+        train_size=int(len(df_labels)*test_size)
+
+        if self.args.split_randomly and self.args.split_randomly:
+            tr_features, ev_features, tr_labels, ev_labels = sklearn.model_selection.train_test_split(df_features, df_labels, test_size=test_size)
+        elif self.args.split_train_test:
+            tr_features, tr_labels, ev_features, ev_labels = df_features[:train_size], df_labels[:train_size], df_features[train_size:], df_labels[train_size:]
+        else:
+            tr_features, tr_labels, ev_features, ev_labels = df_features,df_labels,df_features,df_labels
+    
+
+        print("fitting the model...")
+        self.pipe.fit(tr_features, tr_labels)
+        ev_pred = self.pipe.predict(ev_features)
+
+        # unlog the runtimes (they were previously log transformed in the function clean_data()) 
+        cq=pd.DataFrame()
+        cq["labels"] = np.expm1(ev_labels)
+        cq["pred"] = np.expm1(ev_pred)
+
+        # do the final analysis
+        self.analysis_of_results(cq, list(df_features))
+
+        print("done")
+
+    def analysis_of_results(self, cq, features):
+        # some metrics
+        r2_score = sklearn.metrics.r2_score(cq["labels"], cq["pred"])
+        pearson = scipy.stats.pearsonr(cq["labels"], cq["pred"])
+        mse = sklearn.metrics.mean_squared_error(cq["labels"], cq["pred"])
+
+        print("r2 score: %r" % r2_score)
+        print("pearson: %r" % pearson[0])
+        print("mse: %r" % mse)
+
+        # save model
+        joblib.dump(self.pipe, self.args.model_outfile)
+        print("saved model to: %s" % self.args.model_outfile) 
+
+
+        # save a plot
+        if (self.args.plot_outfile != None):
+            plt.figure(figsize=(10,10))
+            plt.scatter(cq["labels"],cq["pred"])
+            # plt.xlim([0,cq["labels"].max()])
+            # plt.ylim([0,cq["labels"].max()])
+            # plt.plot([0,cq["labels"].max()],[0,cq["labels"].max()], 'r')
+            plt.xlabel("Real Runtime")
+            plt.ylabel("Predicted Runtime")
+            plt.title("Mean predictions")
+            plt.savefig(self.args.plot_outfile)
+            print("saved a plot to: %s" % self.args.plot_outfile)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Get the impact of tool features on it\'s runtime.',
+                                     epilog='Accepts tsv and csv files')
+    parser.add_argument('--filename', dest='filename', action='store', required=True)
+    parser.add_argument("--runtime_label", dest='runtime_label', action='store', default="runtime")
+    parser.add_argument("--split_train_test", dest='split_train_test', action='store', default=True)
+    parser.add_argument("--split_randomly", dest='split_randomly', action='store', default=False)
+    parser.add_argument('--plot_outfile', dest='plot_outfile', action='store', default="plot.png", help='png output file.')
+    parser.add_argument('--model_outfile', dest='model_outfile', action='store', default='model.pkl', help='pkl output file.')
+    args = parser.parse_args()
+    return TrainModel(args)
+
+if __name__ == '__main__':
+    main()
+    exit(0)
